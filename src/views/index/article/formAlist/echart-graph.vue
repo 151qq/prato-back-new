cb9893f0626f8e1fd888634952e14e73f92a9c16
151qq <15164369647@163.com>
2018-03-24 12:07:42 +0800

    config

--- src/views/index/article/formAlist/echart-graph.vue
+++ src/views/index/article/formAlist/echart-graph.vue
@@ -14,19 +14,61 @@ export default {
             formatter: '{b}:{c}',
             hideDelay: 0 
         },
-        series: [{
-          name: '树图',
+        series: [
+        {
+          name: '树图1',
           type: 'tree',
           orient: 'horizontal', // vertical horizontal
-          rootLocation: { x: '10%', y: '60%' }, // 根节点位置  {x: 'center',y: 10}
-          nodePadding: 20, //智能定义全局最小节点间距，不能定义层级节点间距，有点搓。
+          top: '5%',
+          left: '2%',
+          bottom: '5%',
+          right: '2%',
+          label: {
+            position: 'top'
+          },
           symbol: 'circle',
-          symbolSize: [60, 60],
-          data: [{
+          symbolSize: [20, 20],
+          initialTreeDepth: 100,
+          expandAndCollapse: true,
+          animationDuration: 550,
+          animationDurationUpdate: 750,
+          data: [
+          {
             name: '手机',
             value: 6,
             symbol: 'image://http://www.iconpng.com/png/ecommerce-business/iphone.png',
-            children: [{
+            children: [
+              {
+                  name: '小米', //由于label的formatter存在bug，所以无法通过html进行格式化，如果要换行要用\n
+                  value: 4,
+                  symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+                  children: [{
+                    name: '小米11',
+                    value: 10,
+                    symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+                  }]
+              },
+              {
+                name: '苹果',
+                symbol: 'image://http://www.viastreaming.com/images/apple_logo2.png',
+                value: 4
+              },
+              {
+                  name: '小米', //由于label的formatter存在bug，所以无法通过html进行格式化，如果要换行要用\n
+                  value: 4,
+                  symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+                  children: [{
+                    name: '小米11',
+                    value: 10,
+                    symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+                  }]
+              },
+              {
+                name: '苹果',
+                symbol: 'image://http://www.viastreaming.com/images/apple_logo2.png',
+                value: 4
+              },
+              {
                   name: '小米', //由于label的formatter存在bug，所以无法通过html进行格式化，如果要换行要用\n
                   value: 4,
                   symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
@@ -45,7 +87,8 @@ export default {
           }]
         }]
       },
-      mockData: {
+      mockData: [
+      {
           name: '华为',
           value: 4,
           symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
@@ -54,7 +97,17 @@ export default {
             value: 10,
             symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
           }]
-      }
+      },
+      {
+          name: '华为',
+          value: 4,
+          symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+          children: [{
+            name: '小米11',
+            value: 10,
+            symbol: 'image://http://pic.58pic.com/58pic/12/36/51/66d58PICMUV.jpg',
+          }]
+      }]
     }
   },
   mounted () {
@@ -63,31 +116,14 @@ export default {
   methods: {
     // 获取echarts数据
     getData (param) {
-      // console.log(param)
-      // console.log(this.myChart.getOption())
+      console.log(this.myChart.getOption())
       if(!(param.data.children && param.data.children.length > 0)) {
           // 没有数据获取数据
           setTimeout(() => {
-            param.data.children = []
-            param.data.children.push(this.mockData)
+            param.data.children = this.mockData
             this.myChart.setOption(this.myChart.getOption())
           }, 300)
       }
-    },
-    setEcharts () {
-      let datas = this.echartsDate
-      // 设置标题
-      this.option.title.text = datas.title ? datas.title : ''
-      // 设置legend
-      this.option.legend[0].data = datas.categories ? datas.categories : []
-      this.option.series[0].categories = datas.categories ? datas.categories : []
-
-      
-      // 设置关系数据
-      this.option.series[0].data = [].concat(datas.nodes)
-      this.option.series[0].links = [].concat(datas.links)
-
-      this.drawEchart()
     },
     drawEchart () {
       // 使用刚指定的配置项和数据显示图表。
@@ -102,8 +138,8 @@ export default {
 </script>
 <style lang="scss">
   .echar-box {
-    width: 100%;
-    height: 350px;
+    width: 1000px;
+    height: 1000px;
     box-sizing: border-box;
   }
 </style>
